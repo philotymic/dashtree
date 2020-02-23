09b1d1398c943f852f236c3ded89fabac1513ab2
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2020-02-23 02:53:17 +0000

    backend and test script works

--- backend/run-backend.py
+++ backend/run-backend.py
@@ -3,58 +3,43 @@
 # python run-tests.py test-subscriber -- subscriber
 #
 #import ipdb
-import sys, os, asyncio
+import sys, os, asyncio, asgiref.sync
 import prctl, signal
 
 import websockets
-sys.path.append(os.path.join(os.environ['dipole_topdir'], "src"))
+sys.path.append("/home/asmirnov/dipole/src")
 import libdipole
-import libdipole.autoport
+sys.path.append("gen-py")
+import backend
 
-@libdipole.interface
-class TopicSubscriptions:
-    def getTopicPathes(self): pass
-    def getTopicState(self, topic_path): pass
-    def publish(self, topic_path, topic_state): pass
-    def subscribe(self, subscriber_id): pass
-    
-class TopicSubscriptions(TopicSubscriptions)
+class TopicsSubscriptionsI(backend.TopicsSubscriptions):
     def __init__(self):
         self.topics = {} # topic -> text
         self.subscribers = [] # proxies to TopicSubsriber objects
         
-    def getTopicPathes(self):
-        return self.topics.keys()
+    async def getTopicPathes(self):
+        return list(self.topics.keys())
 
-    def getTopicState(self, topic_path):
+    async def getTopicState(self, topic_path):
         return self.topics[topic_path] if topic_path in self.topics else None
 
-    def publish(self, topic_path, topic_state):
+    async def publish(self, topic_path, topic_state):
         print("TopicSubscriptionsI::publish:", topic_path, topic_state, len(self.subscribers))
         self.topics[topic_path] = topic_state
         for subscriber in self.subscribers[:]:
-            subscriber.onTopicStateChange(topic_path, topic_state)
+            await subscriber.onTopicStateChange(topic_path, topic_state)
 
-    def subscribe(self, subscriber_id):
-        print("TopicsSubscriptionsI::subscribe:", subscriber_id)
-        self.subscribers.append(subscriber_id)
+    async def subscribe(self, subscriber_obj_id, ctx):
+        print("TopicsSubscriptionsI::subscribe:", subscriber_obj_id)
+        subscriber_prx = backend.TopicSubscriberPrx(ctx.ws_handler, subscriber_obj_id)
+        self.subscribers.append(subscriber_prx)
         
 if __name__ == "__main__":
-    # https://github.com/seveas/python-prctl -- prctl wrapper module
-    # more on pdeathsignal: https://stackoverflow.com/questions/284325/how-to-make-child-process-die-after-parent-exits
-    prctl.set_pdeathsig(signal.SIGTERM) # if parent dies this child will get SIGTERM
-    
-    xfn_fn = sys.argv[1]
+    object_server = libdipole.ObjectServer()
+    ws_handler_f = libdipole.WSHandlerFactory(object_server)
+    print("adding object topics")
+    object_server.add_object("topics", TopicsSubscriptionsI())
 
-    ws_handler = libdipole.WSHandler();
-    print("adding object topic_subscriptions")
-    ws_handler.object_server.add_object("topics", TopicSubscriptions())
-
-    ws_l = websockets.serve(ws_handler.message_loop, 'localhost', 0)
+    ws_l = websockets.serve(ws_handler_f.server_message_loop, 'localhost', 3456)
     asyncio.get_event_loop().run_until_complete(ws_l)
-    assigned_port = libdipole.autoport.find_ws_port(ws_l)
-    libdipole.__save_assigned_port(assigned_port, xfn)
     asyncio.get_event_loop().run_forever()
-
-        
-
--- backend/run-tests.py
+++ backend/run-tests.py
@@ -1,43 +1,60 @@
 import sys, time
+import asyncio, uuid
+sys.path.append("/home/asmirnov/dipole/src")
+import libdipole
+sys.path.append("gen-py")
+import backend
 
-@libdipole.interface
-class TopicSubscriber:
-    def onTopicStateChange(self): pass
+class TopicSubscriberI(backend.TopicSubscriber):
+    async def onTopicStateChange(self, topic_path, topic_state):
+        print("TopicSubscriberI::onTopicStateChange:", topic_path, topic_state)
 
-class TopicSubscriberI(TopicSubscriber):
-    def onTopicStateChange(self, topic_path, topic_state):
-        print(topic_path, topic_state)
+async def publisher(ws_handler):
+    print("round-robin test publisher")
+    test_topics = ["/a", "/a/b", "/b", "/b/d"]
+    test_topic_states = ["NONE|", "OK|ok", "ERR|system failure"]
+    center_prx = backend.TopicsSubscriptionsPrx(ws_handler, "topics")
+    while 1:
+        for test_topic in test_topics:
+            for test_topic_state in test_topic_states:
+                print("publish:", test_topics, test_topic_states)
+                await center_prx.publish(test_topic, test_topic_state)
+                await asyncio.sleep(5)
 
-async def main()
+async def subscriber(ws_handler):
+    print("entering subscriber")
+    center_prx = backend.TopicsSubscriptionsPrx(ws_handler, "topics")
+    if 0:
+        print("topics:", await center_prx.getTopicPathes())
+        print("topic states:")
+        for topic in await center_prx.getTopicPathes():
+            print(topic, "-->", await center_prx.getTopicState(topic))
+
+    obj = TopicSubscriberI()
+    obj_id = str(uuid.uuid1())
+    ws_handler.object_server.add_object(obj_id, obj)
+    await center_prx.subscribe(obj_id)
+    print("start listening")
+    await asyncio.sleep(1000000)
+    
+async def main():
     test_type = sys.argv[1]
     port = int(sys.argv[2])
-    ws_handler = libdipole.WSHandler();
-    await ws_handler.connect(("localhost", port)):
-    center_prx = TopicsSubscriptionsPrx("topics", ws_handle)
+    object_server = libdipole.ObjectServer()
+    ws_handler = libdipole.WSHandler(object_server);
+    ws_url = f"ws://localhost:{port}"
+    await ws_handler.client_message_loop(ws_url)
     
     if test_type == "test-publisher":
-        # round-robin test publisher
-        test_topics = ["/a", "/a/b", "/b", "/b/d"]
-        test_topic_states = ["NONE|", "OK|ok", "ERR|system failure"]
-        while 1:
-            for test_topic in test_topics:
-                for test_topic_state in test_topic_states:
-                    await center_prx.publish(test_topic, test_topic_state)
-                    await asyncio.sleep(5)
+        await asyncio.create_task(publisher(ws_handler))
     elif test_type == "test-subscriber":
-        print "topics:", center_prx.getTopicPathes()
-        print "topic states:"
-        for topic in center_prx.getTopicPathes():
-            print topic, "-->", await center_prx.getTopicState(topic)
-            print "start listening"
-
-        obj = TopicSubscriberI()
-        obj_id = str(uuid.uuid1())
-        ws_handle.add_object(obj_id, obj)
-        await center_prx.subscribe(obj_id)
+        await asyncio.create_task(subscriber(ws_handler))
     else:
+        print("exception")
         raise Exception("unknown test_type %s" % test_type)
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
-            
+    res = asyncio.run(main())    
+    print("__main__: all done", res)
+    
+    
